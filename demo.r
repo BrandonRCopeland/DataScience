d35afd73d807e8890557d93ec401a7d7dfe4d942
Brandon Copeland <copeland.brandon@gmail.com>
2018-11-01 20:21:59 +0000

    Added Sparklyr to demo.r

--- demo.r
+++ demo.r
@@ -1,7 +1,28 @@
+if (!require("sparklyr")) {
+  install.packages("sparklyr")
+}
+
+# Set options for decimal vs. scientific notation and consold print width
+options(scipen = 999)
+options(width = 200)
+
+library(SparkR)
+library(sparklyr)
 library(dplyr)
-library(lubridate)
+library(tidyr)
+library(ggplot2)
 
-demo.sep <- demo %>% filter(SnapshotDate == "9/30/2018")
-demo.oct <- demo %>% filter(SnapshotDate == "10/31/2018")
+# Start the spark session
+sparkR.session()
 
-x <- "This is another test"
+# Connect the spark session to databricks
+sc <- spark_connect(method = "databricks")
+
+createOrReplaceTempView(as.DataFrame(DataScience::demo), "sdf_demo")
+
+sdf <- tbl(sc, "sdf_demo")
+sdf.sep <- sdf %>% filter(SnapshotDate == "9/30/2018")
+sdf.oct <- sdf %>% filter(SnapshotDate == "10/31/2018")
+
+
+
