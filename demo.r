c287b727e48fb247533b6dac1da0452638e509f1
Brandon Copeland <copeland.brandon@gmail.com>
2018-11-02 17:39:08 +0000

    updating binning and quantizer function

--- demo.r
+++ demo.r
@@ -20,7 +20,7 @@ sc <- spark_connect(method = "databricks")
 
 createOrReplaceTempView(as.DataFrame(DataScience::demo), "sdf_demo")
 
-sdf.old <- tbl(sc, "tbl_engagedusers_2018_07_31") %>% select(CoreApps_Word_UsageDays)
+sdf.old <- tbl(sc, "tbl_engagedusers_2018_07_31") %>% select(CoreApps_Word_UsageDays, CoreApps_Excel_UsageDays)
 sdf.old <- tbl(sc, "tbl_engagedusers_2018_08_31") %>% select(CoreApps_Word_UsageDays)
 
 sdf.sep <- sdf.sep %>% group_by(Week1Usage) %>% summarise(Subs = n_distinct(EntityId)) %>% arrange(Week1Usage)
@@ -35,19 +35,42 @@ head(sdf.old.buckets)
 # bucket 1 with -Inf and max value of max bucket with Inf
 
 Sys.time()
-sdf.buckets <- ft_quantile_discretizer(sdf.old, "CoreApps_Word_UsageDays", "Bin", num_buckets = 10L) %>%
+features <- c("CoreApps_Word_UsageDays", "CoreApps_Excel_UsageDays")
+sdf.buckets <- ft_quantile_discretizer(sdf.old,
+                                       input_cols = features,
+                                       output_cols = get_output_cols(features),
+                                       num_buckets = 10L,
+                                       handle_invalid = "keep") %>%
   mutate(Feature = "CoreApps_Word_UsageDays") %>%
   group_by(Feature, Bin) %>%
-  summarise(count = n(),
+  summarise(Distribution = n(),
             minValue = min(CoreApps_Word_UsageDays),
             maxValue = max(CoreApps_Word_UsageDays)) %>%
-  mutate(minValue = ifelse(Bin == min(Bin), -Inf, minValue),
+  mutate(Distribution_Pct = Distribution / sum(Distribution),
+         minValue = ifelse(Bin == min(Bin), -Inf, minValue),
          maxValue = ifelse(Bin == max(Bin), Inf, maxValue)) %>%
   arrange(Bin) %>%
+  mutate(minValue = ifelse(is.na(lag(maxValue)), minValue, lag(maxValue))) %>%
   collect()
 Sys.time() - start
 
 df.buckets <- sdf.buckets %>% collect()
+
+gather_columns <- tbl_vars(sdf.buckets)[grepl("_Bins", tbl_vars(sdf.buckets))]
+
+tbl_cache(sc, "tbl_engagedusers_2018_07_31")
+sdf.testfunction <- get_feature_bins(tbl(sc, "tbl_engagedusers_2018_07_31"), "CoreApps_Word_UsageDays")
+
+start <- Sys.time()
+df.bins <- sdf.buckets %>%
+  select(contains("Bins")) %>%
+  sdf_gather(gather_columns) %>%
+  group_by(key, value) %>%
+  summarise(Count = n()) %>%
+  collect()
+Sys.time() - start
+
+df.bins <- sdf.buckets.gathered %>% sdf_gather(gather_columns) %>% collect()
 #Need to
 #
 # 1) get data type of data.... numeric or chr
@@ -55,12 +78,50 @@ df.buckets <- sdf.buckets %>% collect()
 # 3) bin numeric via leah's process is.numeric()
 # 4) calculate PSI
 
-get_feture_distributin <- function(sdf, feature) {
-  ft_q
-  sdf.sep <- sdf.sep %>%
-    group_by(Week1Usage) %>%
-    summarise(Subs = n_distinct(EntityId)) %>%
-    arrange(Week1Usage)
+sdf_gather <- function(tbl, gather_cols){
+
+  other_cols <- colnames(tbl)[!colnames(tbl) %in% gather_cols]
+
+  lapply(gather_cols, function(col_nm){
+    tbl %>%
+      select(c(other_cols, col_nm)) %>%
+      mutate(key = col_nm) %>%
+      rename(value = col_nm)
+  }) %>%
+    sdf_bind_rows() %>%
+    select(c(other_cols, 'key', 'value'))
+}
+
+get_output_cols <- function(colnames){
+
+  output_cols <- vector()
+
+  for (col in 1:length(colnames)){
+     output_cols[col] <- paste(colnames[col], "_Bins", sep = "")
+  }
+
+  return(output_cols)
+}
+
+get_feature_bins <- function(sdf, feature_name) {
+
+  #feature_name <- tbl_vars(sdf)[1]
+
+  sdf.temp <- sdf %>%
+    rename(Value = 1) %>%
+    mutate(Feature = feature_name)
+
+  sdf.temp <- ft_quantile_discretizer(sdf.temp, "Value", "Bin", num_buckets = 10L, handle_invalid = "keep") %>% #create 10 buckets
+    group_by(Feature, Bin) %>%
+    summarise(Distribution = n(),
+              minValue = min(Value),     # Get minimum value
+              maxValue = max(Value)) %>%
+    mutate(Distribution_Pct = sum(Distribution),
+           minValue = ifelse(Bin == min(Bin), -Inf, minValue),
+           maxValue = ifelse(Bin == max(Bin), Inf, maxValue)) %>%
+    arrange(Bin) %>%
+    mutate(minValue = ifelse(is.na(lag(maxValue)), minValue, lag(maxValue))) %>%
+    select(Feature, Bin, Distribution, Distribution_Pct, minValue, maxValue)
 
 }
 
