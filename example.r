14af8712dd06ca47361a2fbe0b438902a939e817
Brandon Copeland <brcopela@microsoft.com>
2018-11-07 10:40:00 -0800

    updated expamples to include spark configuration

--- example.r
+++ example.r
@@ -1,31 +1,42 @@
+
+if(!require("devtools")){
+
+}
+if(!require("sparklyr")){
+  install.packages("sparklyr")
+}
+if(!require("SparkR")){
+  install_github("apache/spark@v2.3.0", subdir = "R/pkg")
+}
+
 library(SparkR)
 library(sparklyr)
 library(dplyr)
+
+install_github("BrandonRCopeland/DataScience")
 library(DataScience)
 
-# Get Spark set up.
-sparkR.session()
-sc <- spark_connect(method = "databricks")
+spark_config <- spark_config()
+conf$spark.executor.memory <- "1GB"
+conf$spark.memory.fraction <- 0.9
+conf$spark.executor.cores <- 1
+conf$spark.dynamicAllocation.enabled <- "false"
 
-# Random demo dataset for examples
-#head(demo)
+# Get Spark set up (localsdf)
+sparkR.session()
+sc <- spark_connect(master = "local", version = "2.3.0", config = spark_config)
 
 # Add our demo R dataframe to Spark as a Spark DataFrame
 df <- demo
 
-#try(db_drop_table(sc, "sdf_demo"))
-#copy_to(sc, df, "sdf_demo")
-#sdf.demo <- tbl(sc, "sdf_demo")
+try(db_drop_table(sc, "sdf_demo"))
+copy_to(sc, df, "sdf_demo")
+sdf.demo <- tbl(sc, "sdf_demo")
 
-start <- Sys.time()
 # Create our parameters based off of the demo data.  We'll use the data from
 # September as the expected and October as actual
-sdf.expected <- tbl(sc,"tbl_engagedusers_2018_04_30") %>%
-  mutate(Raw_Score_Weeks = as.numeric(Raw_Score_Weeks)) %>%
-  select("Subscription_Engagement_Segment", "Raw_Score_Weeks")
-sdf.actual <- tbl(sc,"tbl_engagedusers_2018_05_31") %>%
-  mutate(Raw_Score_Weeks = as.numeric(Raw_Score_Weeks)) %>%
-  select("Subscription_Engagement_Segment", "Raw_Score_Weeks")
+sdf.expected <- sdf.demo %>% filter(SnapshotDate == "9/30/2018") %>% select(-one_of("EntityId", "SnapshotDate"))
+sdf.actual <- sdf.demo %>% filter(SnapshotDAte == "10/31/2018") %>% select(-one_of("EntityId", "SnapshotDate"))
 
 sdf_register(sdf.expected, "expected")
 sdf_register(sdf.actual, "actual")
@@ -35,15 +46,7 @@ tbl_cache(sc, "actual")
 
 # Run each function as an example (except sdf_gather)
 df.distribution <- get_feature_distribution(tbl(sc, "expected"), tbl(sc, "actual")) %>% collect()
-df.distribution
-Sys.time() - start
 
-start <- Sys.time()
 df.psi <- get_psi_score(tbl(sc, "expected"), tbl(sc, "actual"), bins = 10L) %>% collect()
-df.psi
-Sys.time() - start
-# Collect the Spark DataFrames into local R DataFrames since they're much smaller
-df.psi_bins <- sdf.psi_bins %>% collect()
-df.psi_scores <- sdf.psi_scores %>% collect()
-df.psi_index_distribution <- sdf.psi_index_distribution %>% collect()
+
 
