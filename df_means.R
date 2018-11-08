d78a6ef18ec6c3722e792632832eaf8e6c6a62b3
Brandon Copeland <brcopela@microsoft.com>
2018-11-08 10:09:35 -0800

    f

---
+++ df_means.R
@@ -0,0 +1,15 @@
+df_means <- function(df, features){
+
+}
+
+sdf_means <- function(sdf, features){
+
+}
+
+df_deltas <- function(expected, actual, features = NULL, join_key = NULL){
+
+}
+
+sdf_deltas <- function(epxected, actual, features = NULL, join_key = NULL){
+
+}
