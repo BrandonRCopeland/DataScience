8054307af8979c8a2ae8db92f990ec98f310489a
Brandon Copeland <brcopela@microsoft.com>
2018-11-30 04:25:43 +0000

    more bins

--- R/df_get_feature_bins.R
+++ R/df_get_feature_bins.R
@@ -10,17 +10,12 @@
 
 #' @param data_ A data.frame containing features that need to be binned.
 #' @param features_ A vector of the numeric feature names to be binned
-#' @param bins The number of bins to create in.  Use the L suffix to ensure the value is an integer.
 #' @return A data.frame containing the feature name, bin number, min value, and max value
 #' @export
-df_get_feature_bins <- function(data_, features_ = NULL, dataType = "numeric", bins_ = NULL) {
+df_get_feature_bins <- function(data_, features_ = NULL, dataType = "numeric") {
 
   if(missing(features_)){
     features_ <- colnames(data_)
-  }
-
-  if(missing(bins_)){
-    bins_ = 10L
   }
 
   if(dataType == "numeric"){
@@ -30,7 +25,7 @@ df_get_feature_bins <- function(data_, features_ = NULL, dataType = "numeric", b
       dplyr::select_if(is.numeric) %>%
       tidyr::gather('feature', 'value') %>%
       dplyr::group_by(feature) %>%
-      dplyr::mutate(bin = ntile(value, bins_)) %>%
+      dplyr::mutate(bin = ntile(value, 10L)) %>%
       dplyr::group_by(feature, bin) %>%
       dplyr::summarise(min = min(value),
                 max = max(value)) %>%
