51b73c38dd407b75109f70f0eced7ea034d0a732
BrandonRCopeland <44583531+BrandonRCopeland@users.noreply.github.com>
2018-12-14 09:42:57 -0800

    Update df_get_feature_bins.R

--- R/df_get_feature_bins.R
+++ R/df_get_feature_bins.R
@@ -18,27 +18,27 @@ df_get_feature_bins <- function(data_, features_, dataType = "numeric") {
   if(dataType == "numeric"){
 
     df.numeric.temp_ <- data_ %>%
-      dplyr::select(features_) %>%
+      dplyr::select(one_of(features_)) %>%
       dplyr::select_if(is.numeric) %>%
       tidyr::gather('feature', 'value') %>%
       dplyr::group_by(feature) %>%
       dplyr::mutate(bin = ntile(value, 10L)) %>%
       dplyr::group_by(feature, bin) %>%
       dplyr::summarise(min = min(value),
-                max = max(value)) %>%
+                       max = max(value)) %>%
       dplyr::filter(row_number() == 1 | min != max) %>% #edge cases where all bins are 0 min and max
       dplyr::mutate(min = ifelse(bin == min(bin, na.rm = TRUE), -Inf, min),
-             max = as.numeric(ifelse(bin == max(bin, na.rm = TRUE), Inf, max))) %>%
+                    max = as.numeric(ifelse(bin == max(bin, na.rm = TRUE), Inf, max))) %>%
       dplyr::arrange(feature, bin) %>%
       dplyr::mutate(bin = as.character(row_number()),
-             DataType = "numeric")
+                    DataType = "numeric")
 
     return(df.numeric.temp_)
   }
 
   if(dataType == "categorical"){
     df.categorical.temp_ <- data_ %>%
-      dplyr::select(features_) %>%
+      dplyr::select(one_of(features_)) %>%
       dplyr::select_if(function(col) is.character(col) | is.factor(col)) %>%
       dplyr::mutate_all(funs(as.character)) %>%
       tidy::gather('feature', 'bin') %>%
