e4d5a2f8fd41714d7c0f307580f38b0232ca0572
Brandon Copeland <brcopland@microsoft.com>
2018-12-27 18:01:18 +0000

    updated magnitude function name.

--- R/df_model_monitoring_magnitude.R
+++ R/df_model_monitoring_magnitude.R
@@ -13,48 +13,48 @@
 #' @param CramersVScore Required: The Cramer's V score for goodness of fit.
 #' @param DegreesOfFreedom Required: degrees of freedom from the ChiSquared test / number of bins - 1
 #' @return The magnitude of Cramer's V score: negligible, low, moderate, or large
-df_cramers_v_magnitude <- function(CramersVScore, DegreesOfFreedom){
+df_model_monitoring_magnitude <- function(CramersVScore, DegreesOfFreedom){
 
   if(is.na(CramersVScore) | is.na(DegreesOfFreedom)){
     return(NA)
   }
 
   if(DegreesOfFreedom <= 2){
-    small  <- .1
-    medium <- .3
-    large  <- .5
+    small  = .1
+    medium = .3
+    large  = .5
   }else if(DegreesOfFreedom == 3){
-    small  <- .071
-    medium <- .212
-    large  <- 354
+    small  = .071
+    medium = .212
+    large  = 354
   }else if(DegreesOfFreedom == 4){
-    small  <- .058
-    medium <- .173
-    large  <- .289
+    small  = .058
+    medium = .173
+    large  = .289
   }else if(DegreesOfFreedom == 5){
-    small  <- .05
-    medium <- .15
-    large  <- .25
+    small  = .05
+    medium = .15
+    large  = .25
   }else if(DegreesOfFreedom == 6){
-    small  <- .045
-    medium <- .134
-    large  <- .224
+    small  = .045
+    medium = .134
+    large  = .224
   }else if(DegreesOfFreedom == 7){
-    small  <- .043
-    medium <- .13
-    large  <- .217
+    small  = .043
+    medium = .13
+    large  = .217
   }else if(DegreesOfFreedom == 8){
-    small  <- .042
-    medium <- .127
-    large  <- .212
+    small  = .042
+    medium = .127
+    large  = .212
   }else if(DegreesOfFreedom == 9){
-    small  <- .042
-    medium <- .125
-    large  <- .209
+    small  = .042
+    medium = .125
+    large  = .209
   }else if(DegreesOfFreedom >= 10){
-    small  <- .041
-    medium <- .124
-    large  <- .207
+    small  = .041
+    medium = .124
+    large  = .207
   }
 
   if(CramersVScore < small){
