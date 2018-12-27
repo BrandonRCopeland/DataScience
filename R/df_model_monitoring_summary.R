5da3dd1cd8667f021369b9485cbeb015a46d5de7
BrandonRCopeland <44583531+BrandonRCopeland@users.noreply.github.com>
2018-12-27 12:31:09 -0800

    Updated value assignments

--- R/df_model_monitoring_summary.R
+++ R/df_model_monitoring_summary.R
@@ -11,48 +11,51 @@
 #' @return A base R dataframe containing the feature, # of bins, Chi Squared Value, P-Value, Cramer's V, Result, and Magnitude.
 #' @export
 df_model_monitoring_summary <- function(distributions){
-
+  
   monitoring_temp = distributions %>%
     dplyr::group_by(feature, DataType) %>%
     dplyr::summarise(bins = dplyr::n()) %>%
     dplyr::mutate(x2 = as.double(NA),
                   dof = as.integer(NA),
                   p = as.double(NA),
-                  cramers_v = as.double(NA)) %>%
+                  cramers_v = as.double(NA),
+                  magnitude = as.character(NA),
+                  result = as.character(NA)) %>%
     dplyr::arrange(dplyr::desc(DataType), feature) %>%
     dplyr::ungroup()
-
+  
   for (row in 1:nrow(monitoring_temp)) {
-
+    
     feature          = unlist(monitoring_temp[row, "feature"])
-    numberOfBins     = monitoring_temp[row, "bins"]
-
+    numberOfBins     = unlist(monitoring_temp[row, "bins"])
+    
     if (numberOfBins - 1 > 0 ) {
-
-      distributions_temp = distributions %>%
-        dplyr::filter(feature == !!feature)
-
-      cs_test = stats::chisq.test(distributions_temp$Actual, p = distributions_temp$Expected_pct, rescale.p = TRUE)
-
-      monitoring_temp[row, "x2"] = cs_test$statistic
-      monitoring_temp[row, "dof"] = cs_test$parameter
-      monitoring_temp[row, "p"] = cs_test$p.value
-
-      ## You can calculate the below manually via this calc:  sqrt(cs_test$statistic / (n * cs_test$parameter))
-      monitoring_temp[row, "cramers_v"] = rcompanion::cramerVFit(x = distributions_temp$Actual, p = distributions_temp$Expected_pct)
+      
+      distributions_temp = distributions[distributions$feature == feature,]
+      
+      cs_test   = stats::chisq.test(distributions_temp$Actual, p = distributions_temp$Expected_pct, rescale.p = TRUE)
+      x2        = as.double(cs_test$statistic)
+      dof       = as.double(cs_test$parameter)
+      pvalue    = as.double(cs_test$p.value)
+      v         = as.double(rcompanion::cramerVFit(x = distributions_temp$Actual, p = distributions_temp$Expected_pct)) ## Manual calc: sqrt(x2 / (n * dof))
+      magnitude = model_monitoring_magnitude(v, dof)
+      result    = ifelse(pvalue > 0, "PASS",
+                         ifelse(magnitude == "negligible",
+                                "PASS",
+                                "FAIL"))
+      
+      monitoring_temp[row, "x2"]        = x2
+      monitoring_temp[row, "dof"]       = dof
+      monitoring_temp[row, "p"]         = pvalue
+      monitoring_temp[row, "cramers_v"] = v
+      monitoring_temp[row, "magnitude"] = magnitude
+      monitoring_temp[row, "result"]    = result
     }
   }
-
+  
   output = monitoring_temp %>%
-    dplyr::mutate(result = ifelse(p > 0.05, "PASS", "FAIL"),
-                  magnitude = df_model_monitoring_magnitude(cramers_v, dof),
-                  result = ifelse(result == "PASS",
-                                  "PASS",
-                                  ifelse(magnitude == "negligible",
-                                         "PASS",
-                                         "FAIL"))) %>%
     dplyr::select(feature, bins, x2, p, cramers_v, result, magnitude) %>%
     dplyr::arrange(result, feature)
-
+  
   return(output)
 }
